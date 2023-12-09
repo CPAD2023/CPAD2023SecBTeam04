88cd232b50eb062ea647c1c6a425ecd2e3f130d3
sharma-jahnvi <811.jahnvi@gmail.com>
2023-12-09 15:39:13 +0530

    caching and search changes

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,28 +1,97 @@
-import React from 'react';
-import {View, TextInput, StyleSheet} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { debounce } from 'lodash';
+import memoize from 'lodash/memoize';
+import React, { useCallback, useState } from 'react';
+import { View, TextInput, Dimensions, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { XMarkIcon } from 'react-native-heroicons/outline';
+import tw from 'twrnc';
+import { fetchEverythingByKeyword, newsSearchIcon } from '../../api/newsDB';
+import Loading from '../components/common/Loading';
+import PoliticalNews from '../components/PoliticalNews';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SearchBar = () => {
+const {width, height} = Dimensions.get('window');
+const memoizedFetchEverything = memoize(fetchEverythingByKeyword);
+
+export default function SearchBar() {
+    const [items, setItems] = useState([]);
+    const navigation = useNavigation();
+
+    const handleSearch = async search => {
+        setLoading(true);
+        console.log(search);
+        if (search && search.length > 2) {
+            try {
+                const cachedData = await AsyncStorage.getItem(search);
+                if (cachedData) {
+                    console.log("Retrieving results from cache...");
+                    setLoading(false);
+                    setItems(JSON.parse(cachedData));
+                } else {
+                    fetchDataAndUpdateCache(search);
+                }
+            } catch (error) {
+                setLoading(false);
+                console.error('AsyncStorage error:', error);
+            }
+        } else {
+            setLoading(false);
+            setItems([]);
+            console.log("No Data incoming...");
+        }
+    }
+
+    const fetchDataAndUpdateCache = (search) => {
+        memoizedFetchEverything(search, 5)
+            .then(data => {
+                console.log("Data incoming...");
+                setLoading(false);
+                if (data && data.articles) {
+                    setItems(data.articles);
+                    AsyncStorage.setItem(search, JSON.stringify(data.articles));
+                }
+            })
+            .catch(error => {
+                setLoading(false);
+                console.error('Error fetching data:', error);
+            });
+    };
+
+    const handleTextDebounce = useCallback(debounce(handleSearch, 400), []);
+    const [loading, setLoading] = useState(false);
+
     return(
-        <View style={styles.container}>
-            <TextInput style={styles.searchInput} placeholder='Search here...' />
-        </View>
+        <SafeAreaView>
+            <View style={{ ...{ width: width/1.05, height: height/15 }, 
+            ...tw `mt-4 mb-4 flex-row justify-between items-center border border-neutral-500 rounded-full` }} >
+                <TextInput onChangeText={handleTextDebounce}
+                    style={{ ...{ width: width/1.05, height: height/15 }, 
+                    ...tw `ml-4 mt-4 mb-4 justify-between items-center rounded-full` }}
+                    placeholder='Search here...' 
+                    placeholderTextColor={'lightgray'}
+                />
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('HomeScreen')}
+                >
+                    <XMarkIcon size={20} color={'lightgray'} style={tw `mr-4`}></XMarkIcon>
+                </TouchableOpacity>
+            </View>
+            {
+                loading ? ( <Loading /> ) : (
+                    items.length > 0 ? (
+                        <ScrollView showsVerticalScrollIndicator={false}
+                            contentContainerStyle={{paddingHorizontal:15}}
+                            style={tw`space-y-3`} >
+                            <PoliticalNews data={items} title="Results" />
+                        </ScrollView>
+                    ) : (
+                        <View style={tw`flex-row justify-center`}>
+                            <Image source={{uri: newsSearchIcon}} style={tw`h-96 w-96`} />
+                        </View>
+                    )
+                )
+            }
+        </SafeAreaView>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: 50,
-        backgroundColor: 'white',
-        borderRadius: 8,
-        justifyContent: 'center'
-    },
-    searchInput: {
-        width: '100%',
-        height: '100%',
-        paddingLeft: 8,
-        fontSize: 16
-    }
-});
-
-export default SearchBar;
--- app/components/screens/HomeScreen.js
+++ app/components/screens/HomeScreen.js
@@ -1,4 +1,3 @@
-import SearchBar from '../SearchBar';
 import Screen from '../common/Screen';
 import FeaturedNews from '../FeaturedNews';
 import BreakingNews from '../BreakingNews';
@@ -6,6 +5,11 @@ import TechNews from '../TechNews';
 import PoliticalNews from '../PoliticalNews';
 import EntertainmentNews from '../EntertainmentNews';
 import useNews from '../../hooks/useNews';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import tw from 'twrnc';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
     const [
@@ -15,10 +19,18 @@ const HomeScreen = () => {
         techNews,
         entertainmentNews,
     ] = useNews();
+    const navigation = useNavigation();
 
     return (
         <Screen>
-            {/* <SearchBar /> */}
+            <StatusBar style="light" />
+            <View style={tw`flex-row justify-between items-center`}>
+                <Text style={tw`text-black text-3xl font-bold ml-20`}>NEWS APP</Text>
+                <TouchableOpacity style={tw`mr-3`} onPress={() => navigation.navigate('SearchBar')}>
+                    <MagnifyingGlassIcon size="30" strokeWidth={2} color="black" />
+                </TouchableOpacity>
+            </View>
+            
             <FeaturedNews data={featuredNews} />
             <TechNews data={techNews} />
             <PoliticalNews data={politicalNews} />
